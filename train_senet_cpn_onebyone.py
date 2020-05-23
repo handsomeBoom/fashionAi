413133b82b7c9ccd505c2688dc0a323d5d715ce1
middleprince <hellangel.ah@gmail.com>
2020-05-23 15:05:32 +0000

    feat: increse warm-up default lr

--- train_senet_cpn_onebyone.py
+++ train_senet_cpn_onebyone.py
@@ -112,7 +112,7 @@ tf.app.flags.DEFINE_float(
     'end_learning_rate', 0.000001,
     'The minimal end learning rate used by a polynomial decay learning rate.')
 tf.app.flags.DEFINE_float(
-    'warmup_learning_rate', 0.00001,
+    'warmup_learning_rate', 0.001,
     'The start warm-up learning rate to avoid NAN.')
 tf.app.flags.DEFINE_integer(
     'warmup_steps', 100,
